192a864a6af0789c29fc0b4f62a6920fc9b2a9b8
ishida <webmaster@joe1cafe.com>
2020-07-26 20:26:25 +0800

    add mapbox leaflet map

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+// @flow
 import React from "react";
 import logo from "./logo.svg";
-import "./App.css";
+import "./App.scss";
+
+import SimpleExample from './LeafletMap';
 
 function App() {
   const handleClick = () => {
@@ -15,22 +18,23 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" onClick={handleClick} />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" onClick={handleClick} />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <SimpleExample />
   );
 }
 
