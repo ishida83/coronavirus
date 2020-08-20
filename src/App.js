1487ff2f6210225d61e7785437c3506d2695a60c
ishida <webmaster@joe1cafe.com>
2020-08-20 23:18:45 +0800

    add react-motion

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 // @flow
 import React from "react";
+import { Motion, spring } from "react-motion";
+
 import logo from "./logo.svg";
 import "./App.scss";
 
 import LeafletMap from "./LeafletMap";
 import MapGlMap from "./MapGlMap";
-import AMap from './AMap';
-import QMap from './QMap';
-import BaiduMap from './BaiduMap';
-
+import AMap from "./AMap";
+import QMap from "./QMap";
+import BaiduMap from "./BaiduMap";
 
 class App extends React.Component {
   state = {
-    mapType: 'Leaflet'
+    mapType: "Leaflet",
   };
   handleClick = () => {
     import("./components/moduleA")
@@ -27,22 +28,22 @@ class App extends React.Component {
 
   switchMapEngine = (mapType) => {
     this.setState({
-      mapType
-    })
-  }
+      mapType,
+    });
+  };
 
   renderMap = () => {
-    switch(this.state.mapType) {
-      case 'Leaflet': 
+    switch (this.state.mapType) {
+      case "Leaflet":
         return <LeafletMap switchMapEngine={this.switchMapEngine} />;
-      case 'MapGl':
-        return <MapGlMap switchMapEngine={this.switchMapEngine}/>;
-      case 'AMap':
-        return <AMap switchMapEngine={this.switchMapEngine}/>;
-      case 'Tencent': 
-        return <QMap switchMapEngine={this.switchMapEngine}/>
-      case 'Baidu': 
-        return <BaiduMap switchMapEngine={this.switchMapEngine}/>
+      case "MapGl":
+        return <MapGlMap switchMapEngine={this.switchMapEngine} />;
+      case "AMap":
+        return <AMap switchMapEngine={this.switchMapEngine} />;
+      case "Tencent":
+        return <QMap switchMapEngine={this.switchMapEngine} />;
+      case "Baidu":
+        return <BaiduMap switchMapEngine={this.switchMapEngine} />;
       default:
         return null;
     }
@@ -51,20 +52,32 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" onClick={handleClick} />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Motion
+          defaultStyle={{ x: -200, opacity: 0 }}
+          style={{ x: spring(0), opacity: spring(1) }}
         >
-          Learn React
-        </a>
-      </header> */}
+          {(style) => (
+            <header className="App-header" style={{ transform: `translateX(${style.x}px)`, opacity: style.opacity }}>
+              <img
+                src={logo}
+                className="App-logo"
+                alt="logo"
+                onClick={this.handleClick}
+              />
+              <p>
+                Edit <code>src/App.js</code> and save to reload.
+              </p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+            </header>
+          )}
+        </Motion>
         {this.renderMap()}
       </div>
     );
