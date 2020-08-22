896fb51ac6167bd1d8b5909f79da87539856a9e7
ishida <webmaster@joe1cafe.com>
2020-08-22 15:55:50 +0800

    add side panel

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@
 import React from "react";
 import { Motion, spring } from "react-motion";
 
+import {
+  BrowserView,
+  MobileView,
+  isBrowser,
+  isMobile,
+  withOrientationChange
+} from "react-device-detect";
+
 import logo from "./logo.svg";
 import "./App.scss";
 
@@ -11,9 +19,28 @@ import AMap from "./AMap";
 import QMap from "./QMap";
 import BaiduMap from "./BaiduMap";
 
+const sidePanelStyle = {
+  position: "fixed",
+  width: "50%",
+  height: "100vh",
+  zIndex: 1,
+  backgroundColor: "white",
+};
+
+const sidePanelMobileStyle = {
+  position: "fixed",
+  height: "50vh",
+  width: "100%",
+  zIndex: 1,
+  bottom: 0,
+  backgroundColor: "white",
+};
+
 class App extends React.Component {
   state = {
-    mapType: "Leaflet",
+    mapType: "Baidu",
+    video: null,
+    showMarkerInfo: false
   };
   handleClick = () => {
     import("./components/moduleA")
@@ -32,27 +59,40 @@ class App extends React.Component {
     });
   };
 
+  showMarkerInfo = (e) => {
+    console.log(e);
+    this.setState({
+      video: e.videoUrl,
+      showMarkerInfo: !!e.videoUrl
+    });
+  };
+
   renderMap = () => {
+    let props = {
+      switchMapEngine: this.switchMapEngine,
+      showMarkerInfo: this.showMarkerInfo,
+    };
     switch (this.state.mapType) {
       case "Leaflet":
-        return <LeafletMap switchMapEngine={this.switchMapEngine} />;
+        return <LeafletMap {...props} />;
       case "MapGl":
-        return <MapGlMap switchMapEngine={this.switchMapEngine} />;
+        return <MapGlMap {...props} />;
       case "AMap":
-        return <AMap switchMapEngine={this.switchMapEngine} />;
+        return <AMap {...props} />;
       case "Tencent":
-        return <QMap switchMapEngine={this.switchMapEngine} />;
+        return <QMap {...props} />;
       case "Baidu":
-        return <BaiduMap switchMapEngine={this.switchMapEngine} />;
+        return <BaiduMap {...props} />;
       default:
         return null;
     }
   };
 
   render() {
+    const { isLandscape, isPortrait } = this.props;
     return (
       <div className="App">
-        <Motion
+        {/* <Motion
           defaultStyle={{ x: -200, opacity: 0 }}
           style={{ x: spring(0), opacity: spring(1) }}
         >
@@ -77,11 +117,41 @@ class App extends React.Component {
               </a>
             </header>
           )}
-        </Motion>
+        </Motion> */}
+        <BrowserView>
+          <Motion
+            defaultStyle={{ x: -100, opacity: 0 }}
+            style={{ x: spring(this.state.showMarkerInfo ? 0 : -100), opacity: spring(this.state.showMarkerInfo ? 1 : 0) }}
+          >
+            {(style) => (
+              <aside
+                style={{...sidePanelStyle, transform: `translateX(${style.x}%)`, opacity: style.opacity }}
+                dangerouslySetInnerHTML={{
+                  __html: this.state.video,
+                }}
+              ></aside>
+            )}
+          </Motion>
+        </BrowserView>
+        <MobileView>
+          <Motion
+            defaultStyle={{ y: 100, opacity: 0 }}
+            style={{ y: spring(this.state.showMarkerInfo ? 0 : 100), opacity: spring(this.state.showMarkerInfo ? 1 : 0) }}
+          >
+            {(style) => (
+              <aside
+                style={{...sidePanelMobileStyle, transform: `translateY(${style.y}%)`, opacity: style.opacity }}
+                dangerouslySetInnerHTML={{
+                  __html: this.state.video,
+                }}
+              ></aside>
+            )}
+          </Motion>
+        </MobileView>
         {this.renderMap()}
       </div>
     );
   }
 }
 
-export default App;
+export default withOrientationChange(App);
